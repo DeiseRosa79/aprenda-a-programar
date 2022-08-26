9acbe48177a889f146e76fccfcf78ee859cc4333
Deise Rosa <deise.psicologa.rh@gmail.com>
2022-08-26 15:22:08 -0300

    pratica 1

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -8,3 +8,8 @@ email = "deise.psicologa.rh@gmail.com"
 profissao = "psicologa"
 idade = 43
 lugar = "nova york"
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(lugar)
